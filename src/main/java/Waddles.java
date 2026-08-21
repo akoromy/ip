430b52b886748693fba72af431cd2b6704fffb24
Romy Ako <akoromy@gmail.com>
2026-08-21 13:44:15 +0700

    Add support for marking tasks as done

--- src/main/java/Waddles.java
+++ src/main/java/Waddles.java
@@ -14,7 +14,7 @@ public class Waddles {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (!input.equals("bye")) {
@@ -24,8 +24,18 @@ public class Waddles {
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                tasks[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[index]);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[index]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
@@ -33,6 +43,7 @@ public class Waddles {
             System.out.println("___________________________________________________________<3");
             input = scanner.nextLine();
         }
+
         System.out.println("___________________________________________________________<3");
         System.out.println("Byeee! See you again soon:)");
         System.out.println("___________________________________________________________<3");
