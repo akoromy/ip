546ae87e12c495bd8744d2f4d23b02665d0e0d88
Romy Ako <akoromy@gmail.com>
2026-08-21 13:14:10 +0700

    Rename main class; Add personality

---
+++ src/main/java/Waddles.java
@@ -0,0 +1,14 @@
+public class Waddles {
+    public static void main(String[] args) {
+        String logo = " (\\_/)\n"
+                + "( •,• )\n"
+                + "( \")_(\")  WADDLES";
+        System.out.println("____________________________________________________________");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Waddles.");
+        System.out.println("How can I help you today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Byeee! See you again soon:)");
+        System.out.println("____________________________________________________________");
+    }
+}
