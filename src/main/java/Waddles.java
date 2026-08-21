7ca3ab33e5e26da5314ea4b1a1c62b0bda6433d6
Romy Ako <akoromy@gmail.com>
2026-08-21 13:27:42 +0700

    Add support for echo

--- src/main/java/Waddles.java
+++ src/main/java/Waddles.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class Waddles {
     public static void main(String[] args) {
         String logo = " (\\_/)\n"
                 + "( •,• )\n"
                 + "( \")_(\")  WADDLES";
-        System.out.println("____________________________________________________________");
+        System.out.println("___________________________________________________________<3");
         System.out.println(logo);
         System.out.println("Hello! I'm Waddles.");
         System.out.println("How can I help you today?");
-        System.out.println("____________________________________________________________");
+        System.out.println("___________________________________________________________<3");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("___________________________________________________________<3");
+            System.out.println(input);
+            System.out.println("___________________________________________________________<3");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("___________________________________________________________<3");
         System.out.println("Byeee! See you again soon:)");
-        System.out.println("____________________________________________________________");
+        System.out.println("___________________________________________________________<3");
+
+        scanner.close();
     }
 }
